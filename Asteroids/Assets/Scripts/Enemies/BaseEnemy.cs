f45301ab149d42c6139e3a74341eff79dbf9dd70
makingsplash <58134895+makingsplash@users.noreply.github.com>
2020-03-27 14:08:55 +0300

    started

---
+++ Asteroids/Assets/Scripts/Enemies/BaseEnemy.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public abstract class BaseEnemy : MonoBehaviour
+{
+    private static int _enemyAmount;
+    // Тоже private ?
+    public static int EnemyAmount
+    {
+        get
+        {
+            return _enemyAmount;
+        }
+
+        protected set
+        {
+            _enemyAmount = value;
+        }
+    }
+}
