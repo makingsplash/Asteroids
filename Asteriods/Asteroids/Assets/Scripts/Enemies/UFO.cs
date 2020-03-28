e654e8a4b2a7a4b9329d734d779fa8d5d36edb68
makingsplash <58134895+makingsplash@users.noreply.github.com>
2020-03-28 11:24:07 +0300

    Switch unity version

---
+++ Asteriods/Asteroids/Assets/Scripts/Enemies/UFO.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UFO : BaseEnemy
+{
+
+    [SerializeField] private float _speed;
+    private GameObject _player;
+    private float _nonAngryFlyTimer = 2;
+    private Vector2 _nonAngryFlyDirection;
+    private bool _isAngry = false;
+    private new Rigidbody2D rigidbody;
+
+
+    private void OnEnable()
+    {
+        if (_player == null)
+            _player = GameObject.FindWithTag("Player");
+
+        PlayerController.OnPlayerEnabled += SetPlayerGameObject;
+
+        rigidbody = GetComponent<Rigidbody2D>();
+
+        // Если заспавнились справа - летим влево и наоборот
+        if (transform.position.x > 0)
+            _nonAngryFlyDirection = -transform.right;
+        else
+            _nonAngryFlyDirection = transform.right;
+    }
+
+    private void OnDisable()
+    {
+        PlayerController.OnPlayerEnabled -= SetPlayerGameObject;
+    }
+
+    private void Update()
+    {
+        // Переход из свободного полёта в преследование
+        if (!_isAngry)
+        {
+            _nonAngryFlyTimer -= Time.deltaTime;
+            if (_nonAngryFlyTimer < 0)
+                _isAngry = true;
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if(!_isAngry)
+        {
+            // Свободный полёт
+            rigidbody.velocity = _nonAngryFlyDirection * _speed * Time.deltaTime;
+        }
+        else
+        {
+            // Если игрок жив, преследуем
+            if (_player != null && _player.activeSelf)
+                rigidbody.velocity = (_player.transform.position - transform.position).normalized * _speed * Time.deltaTime;
+            // Если игрок в процессе возрождения, летим вверх
+            else
+                rigidbody.velocity = transform.up * _speed * Time.deltaTime;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        IDamageable player = collision.gameObject.GetComponent<IDamageable>();
+        if (player != null)
+            DoDamage(player);
+    }
+
+    void SetPlayerGameObject(GameObject player)
+    {
+        this._player = player;
+    }
+
+    public override void GotDamage()
+    {
+        base.GotDamage();
+
+        UIManager.Instance.ChangeScore(40);
+        Destroy(gameObject);
+    }
+}
