cd06c6defbe88f2bcfd9017809b0590ca3fea7eb
makingsplash <58134895+makingsplash@users.noreply.github.com>
2020-04-21 11:15:05 +0300

    fixed dpi problem

---
+++ Asteroids/Assets/Scripts/testfps.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class testfps : MonoBehaviour
+{
+    private int frames;
+    private float currentTimer = 1.0f;
+    private float oneSecond = 1.0f;
+
+    private TextMeshProUGUI text;
+
+
+    private void Start()
+    {
+        text = GetComponent<TextMeshProUGUI>();
+    }
+
+    private void Update()
+    {
+        currentTimer -= Time.deltaTime;
+        frames++;
+
+        if (currentTimer < 0)
+        {
+            currentTimer = oneSecond;
+            text.text = Screen.dpi.ToString() + " " + frames.ToString();
+            frames = 0;
+        }
+    }
+}
