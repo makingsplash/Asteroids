f0519a19b6f666dd0475da49e248b139d081113a
makingsplash <58134895+makingsplash@users.noreply.github.com>
2020-04-23 23:43:36 +0300

    added first version of enemy ui health bar

---
+++ Asteroids/Assets/Scripts/MonoBehaviours/UI/EnemyHealthBar.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHealthBar : MonoBehaviour
+{
+    [SerializeField] private Slider _slider;
+
+    private readonly float _oneChange = 0.025f;
+    private readonly WaitForSeconds _wait = new WaitForSeconds(0.01f);
+
+
+    public void ResizeMaxValue(byte health)
+    {
+        _slider.maxValue = health;
+        _slider.value = health;
+    }
+
+    public IEnumerator DecreaseCurrentValue()
+    {
+        float nextValue = _slider.value - 1;
+        while (_slider.value > nextValue)
+        {
+            _slider.value -= _oneChange;
+            yield return _wait;
+        }
+    }
+}
